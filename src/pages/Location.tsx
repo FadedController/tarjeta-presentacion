c9ed623230cc160d8b6359d29cfcbdbae520c4cb
Axel Padilla <adpadillar25@gmail.com>
2021-07-24 12:16:11 -0600

    Fixed React Warning

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -23,6 +23,7 @@ const Location: React.FC = () => {
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d21001.792634182162!2d2.2825966!3d48.8539373!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701f7e8337b5%3A0xa2cb58dd28914524!2sEiffel%20Tower%2C%20Paris%2C%20France!5e0!3m2!1sen!2smx!4v1627149365887!5m2!1sen!2smx"
           height="300"
           allowFullScreen
+          title="Map"
           className="w-full relative"
           loading="eager"
         ></iframe>
